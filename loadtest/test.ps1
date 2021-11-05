50db5b60ec931836568b7c598bfbebc291caa795
Shad Phillips <shadph@microsoft.com>
2021-11-05 09:14:05 -0500

    new tests

--- loadtest/test.ps1
+++ loadtest/test.ps1
@@ -18,7 +18,7 @@ function Log-Message
 }
 
 
-for($l = 0;$l -lt 6; $l++)
+for($l = 0;$l -lt 12; $l++)
 {
     Log-Message "Outer Loop $l"
     $aciCount = 40
@@ -28,7 +28,7 @@ for($l = 0;$l -lt 6; $l++)
         Log-Message "Createing ACI $i"
         az container create -g k6 -n k6load$i --image loadimpact/k6 --command-line "k6 run --vus 400 --duration 120m raw.githubusercontent.com/shadmsft/k6/main/loadtest/test.js" --restart-policy Never
     }
-
+    Log-Message "Sleeping for an hour after generating traffic."
     Start-Sleep -Seconds 3600
 
     for($i = 0; $i -lt $aciCount;$i++)
@@ -36,7 +36,7 @@ for($l = 0;$l -lt 6; $l++)
         Log-Message "Deleting ACI $i"
         az container delete --resource-group k6 --name k6load$i -y
     }
-
+    Log-Message "Sleeping for an hour after stopping traffic."
     Start-Sleep -Seconds 3600
 }
 
